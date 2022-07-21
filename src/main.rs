5f71d91a8ee331b7c00708d5502920f116e28a25
recardinal <recardinal@outlook.com>
2022-07-21 15:06:21 +0800

    generate info with label

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,119 @@ async fn main() -> octocrab::Result<()> {
     Ok(())
 }
 
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct LabelAlter {
+    id: models::LabelId,
+    name: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct IssueAlter {
+    id: models::IssueId,
+    title: String,
+    number: i64,
+    labels: Vec<LabelAlter>,
+}
+
+#[derive(Serialize, Deserialize, std::default::Default)]
+struct PostInfo {
+    issues: Vec<IssueAlter>,
+    labels: Vec<LabelAlter>,
+}
+
+async fn handle_issues() -> octocrab::Result<()> {
+    let GenerateArgs {
+        token,
+        repo_name,
+        issue_number,
+    } = get_args();
+    let octocrab = init_octocrab(token)?;
+
+    let user = get_user(&octocrab).await?;
+
+    let issue_handler = octocrab.issues(&user.login, &repo_name);
+
+    let issues = issue_handler.list().creator(&user.login).send().await?;
+
+    let issues_alter: Vec<IssueAlter> = issues
+        .clone()
+        .into_iter()
+        .map(|issue| IssueAlter {
+            id: issue.id,
+            title: issue.title,
+            number: issue.number,
+            labels: issue
+                .labels
+                .iter()
+                .map(|label| LabelAlter {
+                    id: label.id,
+                    name: label.name.to_string(),
+                })
+                .collect(),
+        })
+        .collect();
+
+    let labels: Vec<LabelAlter> = issue_handler
+        .list_labels_for_repo()
+        .send()
+        .await?
+        .into_iter()
+        .map(|label| LabelAlter {
+            id: label.id,
+            name: label.name,
+        })
+        .collect();
+
+    // 当前已生成 md 的 issue
+    let current_issues = get_generate_issues().issues;
+
+    let current_issues_map: HashSet<models::IssueId> = current_issues
+        .clone()
+        .into_iter()
+        .map(|issue| issue.id)
+        .collect();
+
+    let issues_to_generate: Vec<models::issues::Issue> = if current_issues.len() == 0 {
+        issues.clone().items
+    } else {
+        issues
+            .clone()
+            .into_iter()
+            .filter(|issue| {
+                !current_issues_map.contains(&issue.id)
+                    || (issue_number.is_some()
+                        && issue.id.into_inner()
+                            == issue_number.as_ref().unwrap().parse::<u64>().unwrap())
+            })
+            .collect()
+    };
+
+    println!("{:#?}", issues_alter);
+
+    // 写入新的 issues 信息
+    let new_generate_info_json = serde_json::to_string(&PostInfo {
+        issues: issues_alter,
+        labels,
+    })
+    .unwrap();
+
+    match get_overwrite_file(GENERATE).write(&new_generate_info_json.as_bytes()) {
+        Err(why) => println!("fail to write new issues info: {}", why),
+        Ok(_) => println!("write new issues info success!"),
+    };
+
+    // 生成md
+    for issue in issues_to_generate {
+        let mut file =
+            get_overwrite_file(Path::new(BASE_PATH).join(issue.title.replace(" ", "-") + ".md"));
+
+        file.write(issue.body.unwrap_or_default().as_bytes())
+            .unwrap();
+    }
+
+    Ok(())
+}
+
 struct GenerateArgs {
     token: String,
     repo_name: String,
@@ -44,102 +157,6 @@ fn init_octocrab(token: String) -> octocrab::Result<Octocrab> {
         .build()
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-struct LabelAlter {
-    id: models::LabelId,
-    name: String,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-struct IssueAlter {
-    id: models::IssueId,
-    title: String,
-    number: i64,
-    labels: Vec<LabelAlter>,
-}
-
-async fn handle_issues() -> octocrab::Result<()> {
-    let GenerateArgs {
-        token,
-        repo_name,
-        issue_number,
-    } = get_args();
-    let octocrab = init_octocrab(token)?;
-
-    let user = get_user(&octocrab).await?;
-
-    let issues = octocrab
-        .issues(&user.login, &repo_name)
-        .list()
-        .creator(&user.login)
-        .send()
-        .await?;
-
-    let issues_alter: Vec<IssueAlter> = issues
-        .clone()
-        .into_iter()
-        .map(|issue| IssueAlter {
-            id: issue.id,
-            title: issue.title.replace(" ", "-"),
-            number: issue.number,
-            labels: issue
-                .labels
-                .iter()
-                .map(|label| LabelAlter {
-                    id: label.id,
-                    name: label.name.to_string(),
-                })
-                .collect(),
-        })
-        .collect();
-
-    // 当前已生成 md 的 issue
-    let current_generate_info = get_generate_issues();
-
-    let current_generate_info_map: HashSet<models::IssueId> = current_generate_info
-        .clone()
-        .into_iter()
-        .map(|issue| issue.id)
-        .collect();
-
-    let issues_to_generate: Vec<models::issues::Issue> = if current_generate_info.len() == 0 {
-        issues.clone().items
-    } else {
-        issues
-            .clone()
-            .into_iter()
-            .filter(|issue| {
-                !current_generate_info_map.contains(&issue.id)
-                    || (issue_number.is_some()
-                        && issue.id.into_inner()
-                            == issue_number.as_ref().unwrap().parse::<u64>().unwrap())
-            })
-            .collect()
-    };
-
-    println!("{:#?}", issues_alter);
-
-    // 写入新的 issues 信息
-    let new_generate_info_json = serde_json::to_string(&issues_alter).unwrap();
-
-    let mut file = get_overwrite_file(GENERATE);
-
-    match file.write(&new_generate_info_json.as_bytes()) {
-        Err(why) => println!("fail to write new issues info: {}", why),
-        Ok(_) => println!("write new issues info success!"),
-    };
-
-    // 生成md
-    for issue in issues_to_generate {
-        let mut file = get_overwrite_file(Path::new(BASE_PATH).join(issue.title + ".md"));
-
-        file.write(issue.body.unwrap_or_default().as_bytes())
-            .unwrap();
-    }
-
-    Ok(())
-}
-
 fn get_overwrite_file<P: AsRef<Path>>(path: P) -> File {
     OpenOptions::new()
         .read(true)
@@ -154,7 +171,7 @@ async fn get_user(octocrab: &Octocrab) -> octocrab::Result<models::User> {
     octocrab.current().user().await
 }
 
-fn get_generate_issues() -> Vec<IssueAlter> {
+fn get_generate_issues() -> PostInfo {
     let path = Path::new(GENERATE);
     let mut generate_info = String::new();
 
@@ -172,5 +189,5 @@ fn get_generate_issues() -> Vec<IssueAlter> {
         Ok(_) => println!("read {} successfully", path.to_string_lossy()),
     }
 
-    return serde_json::from_str::<Vec<IssueAlter>>(&generate_info).unwrap_or_default();
+    return serde_json::from_str::<PostInfo>(&generate_info).unwrap_or_default();
 }
